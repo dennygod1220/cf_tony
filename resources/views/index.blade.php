6e97cc402bcb9adaf6f3a69f8b65f674c0e60c9d
Dennygod <dennygod1220@gmail.com>
2019-01-15 09:53:07 +0800

    First Clear Laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CF_Tony</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
+    <script src="{{ asset('public/js/app.js') }}"></script>
+
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <h2>CF_TONY 活動網站</h2>
+                    <button class="btn btn-outline-dark">TEST Bootstrap</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
